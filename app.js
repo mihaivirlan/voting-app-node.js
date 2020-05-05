006acd5e40ea35915619243d778131df0b1c733d
mihail.virlan <mihail.virlan@esolutions.ro>
2020-05-05 13:10:32 +0300

    adding the ability to post data to the file

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
     app = express();
 
-const fs = require('fs');
+const fs = require('fs'),
+    bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -12,6 +18,34 @@ app.get("/poll", (req, res) => {
         res.send(data);
     })
 })
+
+app.post("/vote/new", (req, res) => {
+    if (req.body.linux === 'on') {
+        choosePollOption(req, res, 'linux');
+    } else if (req.body.macos === 'on') {
+        choosePollOption(req, res, 'macos');
+    } else if (req.body.windows === 'on') {
+        choosePollOption(req, res, 'windows');
+    } else {
+        res.redirect('/?incorrect + input');
+    }
+})
+
+function choosePollOption(req, res, topic) {
+    let poll = {};
+    fs.readFile(__dirname + '/poll.json', 'utf8', function (err, data) {
+        poll = JSON.parse(data);
+        poll[topic] += 1;
+        console.log(poll);
+        fs.writeFile(__dirname + '/poll.json',
+        JSON.stringify(poll), function (err, data) {
+            console.log(err);
+            console.log(data);
+            res.status(200).send('want to <a href="/">vote</a> again');
+        })
+    })
+    console.log(poll);
+}
 
 app.listen(3001, () => {
     console.log('listen on port 3001');
